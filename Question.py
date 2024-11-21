686a82c98f7e064b381f209c869042e20523d9c5
Ritikithub <ankursingh4330@gmail.com>
2024-11-21 21:55:00 +0530

    Question.py

---
+++ Question.py
@@ -0,0 +1,53 @@
+questions_by_topic = {
+    "DSA": [
+        {
+            "question": "Which of the following sorting algorithms can be used to sort a random linked list with minimum time complexity??",
+            "options": ["Insertion Sort", "Quick Sort", "Heap Sort", "Merge Sort"],
+            "answer": "Merge Sort"
+        },
+        {
+            "question": "In the worst case, the number of comparisons needed to search a singly linked list of length n for a given element is?",
+            "options": ["log(2*n)", "n/2", "log(2*n) -1", "n"],
+            "answer": "n"
+        },
+        {
+            "question": "Which one of the following is an application of Stack Data Structure?",
+            "options": ["Managing function calls", "The stock span problem", "Arithmetic expression evaluation", "All of the above"],
+            "answer": "All of the above"
+        }
+    ],
+    "DBMA": [
+        {
+            "question": "What does SQL stand for?",
+            "options": ["Structured Query Language", "Super Quantum Logic", "Stylish Query Lingo", "Server Query Lanuage"],
+            "answer": "Structured Query Language"
+        },
+        {
+            "question": "What is the name of the SQL database that comes distributed with Python?",
+            "options": ["PySQL", "PostgreSQL", "SQLite", "MySQL"],
+            "answer": "SQLite"
+        },
+        {
+            "question": "A view of database that appears to an application program is known as?",
+            "options": ["Schema", "Subschema", "Virtual table", "Index Table"],
+            "answer": "Subschema"
+        },
+    ],
+    "PYTHON": [
+        {
+            "question": "What does the 'print' function do in Python?",
+            "options": ["It displays text or variables to the console", "It performs mathematical calculations", "It defines a new function", "It creates a loop"],
+            "answer": "It displays text or variables to the console"
+        },
+        {
+            "question": "Which Python function is used to get the length of a list?",
+            "options": ["len()", "count()", "size()", "length()"],
+            "answer": "len()"
+        },
+        {
+            "question": "Which Python data type is mutable?",
+            "options": ["List", "Tuple", "Set", "String"],
+            "answer": "List"
+        },
+    ]
+}
